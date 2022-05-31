374d54036d41b23ab5ac6967579996e3ab5df535
magicaldave <smokydave@icloud.com>
2022-05-30 23:46:46 -0500

    Add: 102-zombie, which is a C program that created 5 zombie processes.

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -2,15 +2,25 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
+
+/**
+ * main - create 5 zombie processes and print a string for each
+ * Return: always 0
+ */
+
 int infinite_while(void);
 
-int main ()
+int main(void)
 {
 	fork() && fork() || fork();
 	if (fork() == 0)
 		printf("Zombie process created, PID: %d\n", getpid());
 	infinite_while();
 }
+/**
+ * infinite_while - An eternal loop
+ * Return: Always 0
+ */
 int infinite_while(void)
 {
 	while (1)
